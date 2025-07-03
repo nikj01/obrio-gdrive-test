07d0cd413c2b143122691983e96b6f2458776aa2
Nick <nik.jurawlyow2001@gmail.com>
2025-07-03 12:18:37 +0300

    add: setting up checkout of environment variables

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get("SERVICE_PORT");
+
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalPipes(new ValidationPipe(ValidationPipeConfig));
+
+  // SwaggerModule.setup("swagger", app, getDocumentFactory(app), {
+  //   jsonDocumentUrl: "swagger/json",
+  // });
+
+  await app.listen(port ?? 3000);
+  Logger.log(`🚀 Application is running on the port ${port}`);
 }
 bootstrap();
